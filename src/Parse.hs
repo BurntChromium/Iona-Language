ffc9690da4e24759d18f690ca2961c8cdf6389d2
Burnt Chrome <BurntChromium@outlook.com>
2024-06-22 01:04:25 -0400

    numeric literal parser

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,7 +1,26 @@
 module Parse where
 
+import Text.Read(readEither)
+
 import Lex (Token(..), Symbol(..))
-import Errors(Problem(..), ProblemClass(..))
+import Errors(Problem(..), ProblemClass(..), quickProblem)
+
+-- | Built-in data types
+data PrimitiveDataType = PrimitiveInt Int | PrimitiveFloat Float | PrimitiveStr String | PrimitiveBool Bool deriving (Show, Eq)
+
+-- Handle number literals (caller is assumed to only invoke on tokens of the right symbol type)
+-- We treat anything with a "." in it as a float, otherwise it's an integer
+-- TODO: we probably want `Parser = Either [Problem] AST` and this should return Parser
+parseNumberLiteral :: Token -> Either Problem PrimitiveDataType
+parseNumberLiteral t
+    | '.' `elem` str t = 
+        case readEither (str t) :: Either String Float of
+            Right f -> Right (PrimitiveFloat f)
+            Left err -> Left (quickProblem Error (pos t) ("Invalid float literal: " ++ err))
+    | otherwise = 
+        case readEither (str t) :: Either String Int of
+            Right i -> Right (PrimitiveInt i)
+            Left err  -> Left (quickProblem Error (pos t) ("Invalid integer literal: " ++ err))
 
 -- Iterate over a list of tokens until an element is found
 -- This is sort of like `span` but doesn't require writing a custom predicate for matching generic patterns
@@ -18,7 +37,7 @@ takeUntil (t:ts) acceptable terminal
       -- Include the token and continue
       rest <- takeUntil ts acceptable terminal
       return (t : rest)
-  | otherwise = 
+  | otherwise =
       -- Create a problem for the unexpected token
         let problem = Problem {
             cls = Error,
