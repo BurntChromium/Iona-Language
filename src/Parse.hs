48d109c40d00b0dd8980ee6cfeb3b85ff400f8a6
Burnt Chrome <BurntChromium@outlook.com>
2024-06-26 00:27:46 -0400

    stash

--- src/Parse.hs
+++ src/Parse.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Parse where
 
+import Control.Applicative
+import Control.Monad (void)
+import Data.Maybe (maybeToList)
 import Text.Read(readEither)
 
 import Lex (Token(..), Symbol(..))
@@ -10,11 +14,11 @@ import Errors(Problem(..), ProblemClass(..), quickProblem)
 -- ==================== Data Schema ====================
 
 -- | Built-in data types
-data PrimitiveDataType = 
-    PrimitiveInt Int | 
-    PrimitiveFloat Float | 
-    PrimitiveStr String | 
-    PrimitiveBool Bool 
+data PrimitiveDataType =
+    PrimitiveInt Int |
+    PrimitiveFloat Float |
+    PrimitiveStr String |
+    PrimitiveBool Bool
     deriving (Show, Eq)
 
 -- | Valid properties for a variable
@@ -36,18 +40,18 @@ data Variable = Variable {
 type Scope = (String, [String])
 
 -- | AST nodes: only some are true (recursive) trees because statements like import or property lists are flat
-data AST = 
-    Import Scope ImportStmt | 
-    DeclareFn Scope Function [AST] | 
-    DeclareVar Scope Variable [AST] | 
-    Expression Scope ExpressionStmt | 
-    FnProperty Scope [FunctionProperty] | 
-    FnPermission Scope [Permission] 
+data AST =
+    Import Scope ImportStmt |
+    DeclareFn Scope Function [AST] |
+    DeclareVar Scope Variable [AST] |
+    Expression Scope ExpressionStmt |
+    FnProperty Scope [FunctionProperty] |
+    FnPermission Scope [Permission]
     deriving (Show, Eq)
 
 data ImportStmt = NodeImport {
     file :: String,
-    items :: Maybe [String]
+    items :: [String]
 } deriving (Show, Eq)
 
 -- | Core data for a function
@@ -63,28 +67,66 @@ data ExpressionStmt = Literal PrimitiveDataType | Method String [ExpressionStmt]
 
 -- ==================== Parsers ====================
 
-type Parser = [Token] -> Either Problem (AST, [Token])
+-- | A parser can return multiple types, a Variable, an AST, etc.
+type Parser a = [Token] -> Either Problem (a, [Token])
+
+-- Parse a token against a predicate
+tokenSatisfying :: (Token -> Bool) -> Parser Token
+tokenSatisfying pred = Parser $ \case
+    t:ts | pred t -> Right (t, ts)
+    t:ts | not (pred t) -> Left quickProblem Error (pos t) ("unexpected token: " ++ str t)
+
+-- | Flipped version of the <$> (fmap) operator 
+(<&>) :: Functor f => f a -> (a -> b) -> f b
+(<&>) = flip (<$>)
+
+-- Parse identifier token and return the string
+parseIdentifier :: Parser String
+parseIdentifier = tokenSatisfying (\t -> sym t == Identifier) <&> str
+
+-- Parse a symbol, but just checking instead of returning
+parseSymbol :: Symbol -> Parser ()
+parseSymbol s = tokenSatisfying (\t -> sym t == s)
+
+option :: a -> Parser a -> Parser a
+option fallback parser = parser <|> pure fallback
+
+parseSome :: Parser a -> Parser [a]
+parseSome parser = (:) <$> parser <*> many parser
+
+parseMany :: Parser a -> Parser [a]
+parseMany parser = some parser <|> pure []
 
 -- Handle number literals (caller is assumed to only invoke on tokens of the right symbol type)
 -- We treat anything with a "." in it as a float, otherwise it's an integer
 -- TODO: we probably want `Parser = Either [Problem] AST` and this should return Parser
 parseNumberLiteral :: Token -> Either Problem PrimitiveDataType
 parseNumberLiteral t
-    | '.' `elem` str t = 
+    | '.' `elem` str t =
         case readEither (str t) :: Either String Float of
             Right f -> Right (PrimitiveFloat f)
             Left err -> Left (quickProblem Error (pos t) ("Invalid float literal: " ++ err))
-    | otherwise = 
+    | otherwise =
         case readEither (str t) :: Either String Int of
             Right i -> Right (PrimitiveInt i)
             Left err  -> Left (quickProblem Error (pos t) ("Invalid integer literal: " ++ err))
+
+parseVariableDeclaration :: Parser Variable
+parseVariableDeclaration = do
+    void $ symbol VarDeclare
+    name <- identifier
+    void $ symbol FieldSep
+    vType <- dataType
+    vProps <- option [] (parseSymbol FieldSep *> many identifier)
+    void $ parseSymbol Equals
+    return $ Variable name vType (map parseVariableProperty vProps)
 
 -- ==================== Utilities ====================
 
 -- TODO: replace list with sequence so I can push to the back more efficiently
 -- | A modification of `span` that terminates on a *specific* criteria instead of anything outside the predicate
 spanUntil :: ([a], [a]) -> (a -> Bool) -> (a -> Bool) -> Either a ([a], [a])
-spanUntil stream predicate terminal 
+spanUntil stream predicate terminal
     | null (fst stream) = Right stream
     | terminal (head (fst stream)) = Right (tail (fst stream), snd stream ++ [head (fst stream)])
     | predicate (head (fst stream)) = spanUntil (tail (fst stream), snd stream ++ [head (fst stream)]) predicate terminal
